b011a3e9f2b1e646a1204b33775563454682bfc8
Eric Butler <idealeric@gmail.com>
2016-04-23 17:49:51 -0400

    Add File Prompt API to File

--- src/file/index.js
+++ src/file/index.js
@@ -1,10 +1,30 @@
 /* @flow */
+import { remote } from 'electron';
+
 import { readFile as fsReadFile } from 'fs';
-import { promisify } from 'bluebird';
-import type Promise from 'bluebird';
+import Promise, { promisify } from 'bluebird';
 
 export function escapeFilename(filename: string): string {
   return filename.replace(/\//g, '%2F');
+}
+
+export function getFilenameFromUserPrompt(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+      properties: ['openFile'],
+      title: 'Open XPDL',
+      filters: [
+        { name: 'XPDLs', extensions: ['xpdl'] },
+        { name: 'All Files', extensions: ['*'] },
+      ],
+    }, filenames => {
+      if (filenames && filenames.length) {
+        resolve(filenames[0]);
+      } else {
+        reject(new Error('No filename was selected.'));
+      }
+    });
+  });
 }
 
 export const readFile: (fn: string) => Promise<Buffer> = promisify(fsReadFile);
--- src/file/test/index.spec.js
+++ src/file/test/index.spec.js
@@ -1,7 +1,8 @@
 /* eslint-env mocha */
 import expect from 'unexpected';
 
-import { escapeFilename } from '../../file';
+import { escapeFilename, getFilenameFromUserPrompt,
+         __RewireAPI__ as rewire } from '../../file';
 
 describe('File', () => {
 
@@ -14,5 +15,49 @@ describe('File', () => {
     });
 
   }); // end escapeFilename describe
+
+  describe('getFilenameFromUserPrompt', () => {
+
+    let showOpenDialogCb;
+
+    before(() => {
+      const fakeWindow = {};
+      const fakeRemote = {
+        getCurrentWindow() {
+          return fakeWindow;
+        },
+        dialog: {
+          showOpenDialog(win, opts, cb) {
+            expect(win, 'to be', fakeWindow);
+            showOpenDialogCb = cb;
+          },
+        },
+      };
+      rewire.__Rewire__('remote', fakeRemote);
+    });
+
+    after(() => {
+      rewire.__ResetDependency__('remote');
+    });
+
+    it('should resolve with a single filename the user selected.', () => {
+      const prom = getFilenameFromUserPrompt()
+        .then(filename => {
+          expect(filename, 'to be', 'userfilename');
+        });
+      showOpenDialogCb(['userfilename']);
+      return prom;
+    });
+
+    it('should reject with a no filename error when user cancels.', () => {
+      const prom = getFilenameFromUserPrompt()
+        .catch(err => {
+          expect(err.message, 'to be', 'No filename was selected.');
+        });
+      showOpenDialogCb(undefined);
+      return prom;
+    });
+
+  }); // end getFilenameFromUserPrompt describe
 
 });
