94f337cc97f38f1e47579fa8f0a193d8b51be769
Eric Butler <idealeric@gmail.com>
2016-04-23 21:47:04 -0400

    Add ToolBar Component

---
+++ src/components/ToolBar/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import classnames from 'classnames';
+
+import styles from './styles/ToolBar';
+
+export default class ToolBar extends Component {
+  static propTypes = {
+    buttons: ImmutablePropTypes.list.isRequired,
+  }
+
+  static listClassNames = classnames(styles.toolbar, 'columns')
+
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  onClick(index, evt) {
+    const button = this.props.buttons.get(index);
+    if (!button || button.disabled || !button.onClick) {
+      return;
+    }
+    button.onClick(evt);
+  }
+
+  render() {
+    const { buttons } = this.props;
+    return (
+      <ul className={ToolBar.listClassNames}>
+        {buttons.map((button, index) =>
+          <li key={index}
+            className={classnames('material-icons', {
+              [styles.buttonDisabled]: button.disabled,
+            })}
+            onClick={this.onClick.bind(this, index)}>
+            {button.icon}
+          </li>)}
+      </ul>
+    );
+  }
+}
