d6470e98df29c7b468343498a54dcf546e721b70
Eric Butler <idealeric@gmail.com>
2016-04-25 21:29:10 -0400

    Add Open File Button to Empty View

--- src/containers/EmptyFlowstick/index.js
+++ src/containers/EmptyFlowstick/index.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import classnames from 'classnames';
+import Button from 'react-toolbox/lib/button';
 
 import styles from './styles/EmptyFlowstick';
+import { openFileDialog } from '../../actions/filedialog';
 
-export default class EmptyFlowstick extends Component {
-  static classNames = classnames('column', 'columns', 'vert-align', styles.main)
+class EmptyFlowstick extends Component {
+  static propTypes = {
+    fileDialogIsOpen: PropTypes.bool.isRequired,
+    openFileDialog: PropTypes.func.isRequired,
+  }
+
+  static classNames = classnames(
+    'column', 'columns', 'vert-align', 'vert-columns', styles.main)
 
   render() {
+    const { fileDialogIsOpen, openFileDialog } = this.props;
     return (
       <div className={EmptyFlowstick.classNames}>
         <Helmet title="Flowstick" />
-        <h1 className="column">
+        <h1 className="column" style={{ marginBottom: 14 }}>
           <i className="material-icons">sentiment_very_satisfied</i><br />
           Welcome to Flowstick!<br />
           What a beautiful day it is to edit some XPDL.
         </h1>
+        <Button label="Open a File" className="column" raised accent
+          disabled={fileDialogIsOpen}
+          onClick={openFileDialog} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    fileDialogIsOpen: Boolean(state.fileDialog.open),
+  };
+};
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    openFileDialog,
+  }, dispatch);
+}
+
+const connectedEmptyFlowstick = connect(
+  mapStateToProps, mapDispatchToProps)(EmptyFlowstick);
+export default connectedEmptyFlowstick;
